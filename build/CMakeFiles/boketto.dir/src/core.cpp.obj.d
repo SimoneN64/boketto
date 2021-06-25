fbeea27956799546a34b03aa74b51b0006c08b3f
CocoSimone <simococo03@gmail.com>
2021-06-25 17:26:55 +0200

    made simple wrapper class around SDL

--- build/CMakeFiles/boketto.dir/src/core.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/boketto.dir/src/core.cpp.obj: \
- C:/Users/simone.coco/Desktop/Boketto/src/core.cpp
--- build/CMakeFiles/boketto.dir/src/core/cpu.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/boketto.dir/src/core/cpu.cpp.obj: \
- C:/Users/simone.coco/Desktop/Boketto/src/core/cpu.cpp
