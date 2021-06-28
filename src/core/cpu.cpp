570ce0667facee03ab9b45d3456fceaf73d59871
CocoSimone <simococo03@gmail.com>
2021-06-28 17:16:06 +0200

    stepping incorrectly

--- src/core/cpu.cpp
+++ src/core/cpu.cpp
@@ -9,20 +9,16 @@ Cpu::Cpu(std::string bios) : mem(bios) {
 
 void Cpu::step() {
   if(cpsr.thumb) {
-    gpr[PC] &= ~1;
-    auto instruction = mem.ReadHalf(gpr[PC]);
+    auto instruction = mem.ReadHalf(next_pc);
     gpr[PC] += 2;
-    next_pc += 4;
+    next_pc += 2;
     (this->*thumb_lut[instruction >> 6])(instruction);
   } else {
-    gpr[PC] &= ~3;
-    auto instruction = mem.ReadWord(gpr[PC]);
+    auto instruction = mem.ReadWord(next_pc);
     gpr[PC] += 4;
-    next_pc += 8;
+    next_pc += 4;
     if(GetCondition(instruction >> 28)) {
       (this->*arm_lut[((instruction >> 16) & 0xFF0) | ((instruction >> 4) & 0xF)])(instruction);
-    } else {
-      gpr[PC] += 4;
     }
   }
 }
