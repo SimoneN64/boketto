0a1142d18d6edc51f9adc4e15e3d0502f2e0875e
CocoSimone <simococo03@gmail.com>
2021-06-28 17:25:54 +0200

    stepping correctly

--- src/core/cpu.cpp
+++ src/core/cpu.cpp
@@ -5,17 +5,20 @@ namespace boketto::core
 Cpu::Cpu(std::string bios) : mem(bios) {
   GenerateARMLut();
   GenerateThumbLut();
+  cpsr.thumb = 0;
+  gpr[PC] = 0x8000000;
+  gpr[PC] = 0x8000004;
 }
 
 void Cpu::step() {
   if(cpsr.thumb) {
-    auto instruction = mem.ReadHalf(next_pc);
-    gpr[PC] += 2;
+    auto instruction = mem.ReadHalf(gpr[PC]);
+    gpr[PC] = next_pc;
     next_pc += 2;
     (this->*thumb_lut[instruction >> 6])(instruction);
   } else {
-    auto instruction = mem.ReadWord(next_pc);
-    gpr[PC] += 4;
+    auto instruction = mem.ReadWord(gpr[PC]);
+    gpr[PC] = next_pc;
     next_pc += 4;
     if(GetCondition(instruction >> 28)) {
       (this->*arm_lut[((instruction >> 16) & 0xFF0) | ((instruction >> 4) & 0xF)])(instruction);
