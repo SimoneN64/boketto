4c2a61f2ce66448db8ccc2c624c56952d15e3cc9
simuuz <simococo03@gmail.com>
2021-07-16 20:04:29 +0200

    simplified get bit range and get bit by a lot (thanks dillon)

--- include/util/bit.h
+++ include/util/bit.h
@@ -1,22 +1,5 @@
 #pragma once
 #include "common.h"
-#define GET_BIT_RANGE(type) type get_bit_range_##type(type value, u8 start, u8 end)
-#define SET_BIT_RANGE(type) void set_bit_range_##type(type* value, u8 start, u8 end)
-#define GET_BIT(type) type get_bit_##type(type value, u8 pos)
-#define SET_BIT(type) void set_bit_##type(type* value, u8 pos)
-
-GET_BIT_RANGE(u8);
-GET_BIT_RANGE(u16);
-GET_BIT_RANGE(u32);
-
-SET_BIT_RANGE(u8);
-SET_BIT_RANGE(u16);
-SET_BIT_RANGE(u32);
-
-GET_BIT(u8);
-GET_BIT(u16);
-GET_BIT(u32);
-
-SET_BIT(u8);
-SET_BIT(u16);
-SET_BIT(u32);
+#define mask(len) (1 << ((len) - 1))
+#define bits(n, lo, hi) (((n) >> (lo)) & mask((hi) - (lo)))
+#define bit(n, pos) (((n) >> (pos)) & 1)
