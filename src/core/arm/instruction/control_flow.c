24a3027fbf8797c5067e1144dd3517a047901e58
simuuz <simococo03@gmail.com>
2021-07-20 00:44:12 +0200

    Fixed stepping

--- src/core/arm/instruction/control_flow.c
+++ src/core/arm/instruction/control_flow.c
@@ -1,11 +1,10 @@
 #include "control_flow.h"
 
 void branch(registers_t* registers) {
-  u32 instruction = registers->instruction;
-  bool link = (instruction >> 24) & 1;
-  u32 addr = sex((s32)instruction, 24) << 2;
+  bool link = (registers->instruction >> 24) & 1;
+  s32 addr = sex((s32)registers->instruction, 24) << 2;
   
-  printf("b%s %08X\n", link ? "l" : "", addr);
+  printf("b%s %d\n", link ? "l" : "", addr);
   
   if(link) {
     registers->gpr[LR] = registers->gpr[PC] - 4;
--- src/core/cpu.c
+++ src/core/cpu.c
@@ -7,14 +7,14 @@ void init_cpu(cpu_t* cpu) {
 
 void step_cpu(cpu_t* cpu, mem_t* mem) {
   if(cpu->regs.cpsr.thumb) {
+    cpu->regs.gpr[PC] += 2;
     u16 instruction = read_16(mem, cpu->regs.gpr[PC]);
     cpu->regs.instruction = instruction;
-    cpu->regs.gpr[PC] += 2;
     (cpu->thumb_lut[instruction >> 6])(&cpu->regs);
   } else {
+    cpu->regs.gpr[PC] += 4;
     u32 instruction = read_32(mem, cpu->regs.gpr[PC]);
     cpu->regs.instruction = instruction;
-    cpu->regs.gpr[PC] += 4;
     if(get_condition(cpu->regs.cpsr, instruction >> 28)) {
       (cpu->arm_lut[((instruction >> 16) & 0xFF0) | ((instruction >> 4) & 0xF)])(&cpu->regs);
     }
